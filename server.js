57aecf6a380b122d1e18c3fcd56bd6203ff078e9
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-10-11 18:51:09 -0600

    deleted .gitignore

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
 
-app.get('/', (req, res) => res.send(''));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Hello'));
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
@@ -14,12 +20,13 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
-app.get('api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'db/db.json'))
-);
+app.get('api/notes', (req, res) => res.json(path.join(__dirname, db)));
 
-app.post('api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'db/db.json'))
+app.post(
+  'api/notes',
+  (req, res) => res.sendFile(console.log('received'))
+  // (path.join
+  // __dirname, db)));
 );
 
 app.listen(PORT, () =>
