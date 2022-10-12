df73a64c566c816e3f51ac4d5e6a228e74e4c7d6
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-10-11 20:31:09 -0600

    updated get and post requests

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db/db.json');
+
 const fs = require('fs');
-
 const app = express();
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send(''));
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.get('api/notes', (req, res) => res.json(path.join(__dirname, db)));
-
-app.post(
-  'api/notes',
-  (req, res) => res.sendFile(console.log('received'))
-  // (path.join
-  // __dirname, db)));
+app.get('/notes', (req, res) =>
+  res.json(path.join(__dirname, './public/notes.html'))
 );
+
+// app.post('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, './public/notes.html'))
+// );
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
